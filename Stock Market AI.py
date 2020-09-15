156c59ac2f338537ce7ea5b6ed160c29b908008e
iandickson222 <53797442+iandickson222@users.noreply.github.com>
2020-09-14 20:59:07 -0700

    Update Stock Market AI.py

--- Stock Market AI.py
+++ Stock Market AI.py
@@ -9,103 +9,105 @@ import requests
 from bs4 import BeautifulSoup
 
 
-#get data
-shiller_pe = requests.get("https://www.multpl.com/shiller-pe/table/by-month")
-shiller_soup = BeautifulSoup(shiller_pe.content, 'html.parser')
-shiller_table = shiller_soup.find(id="datatable")
-shiller_dates, shiller_values = shiller_table.select(".left"), shiller_table.select(".right")
-shiller_dates, shiller_values = [date.get_text() for date in shiller_dates], [value.get_text() for value in shiller_values]
-shiller_dates.pop(0), shiller_values.pop(0)
-shiller_values = [float(value.replace("\n", "").replace(',', '')) for value in shiller_values]
-shiller_dates, shiller_values = shiller_dates[1500::-1], shiller_values[1500::-1]
+def get_data(url, length):
+    url_html = requests.get(url)
+    soup = BeautifulSoup(url_html.content, 'html.parser')
+    table = soup.find(id="datatable")
+    dates, values = table.select(".left"), table.select(".right")
+    dates, values = [date.get_text() for date in dates], [value.get_text() for value in values]
+    dates.pop(0), values.pop(0)
+    dates.pop(0), values.pop(0)
+    values = [float(value.replace("\n", "").replace(',', '')) for value in values]
+    dates, values = dates[length::-1], values[length::-1]
+    return dates, values
 
-prices = requests.get("https://www.multpl.com/s-p-500-historical-prices/table/by-month")
-prices_soup = BeautifulSoup(prices.content, 'html.parser')
-prices_table = prices_soup.find(id="datatable")
-prices_dates, prices_values = prices_table.select(".left"), prices_table.select(".right")
-prices_dates, prices_values = [date.get_text() for date in prices_dates], [value.get_text() for value in prices_values]
-prices_dates.pop(0), prices_values.pop(0)
-prices_values = [float(value.replace("\n", "").replace(',', '')) for value in prices_values]
-prices_dates, prices_values = prices_dates[1500::-1], prices_values[1500::-1]
+prices_dates, prices_values = get_data("https://www.multpl.com/s-p-500-historical-prices/table/by-month", 1500)
+shiller_dates, shiller_values = get_data("https://www.multpl.com/shiller-pe/table/by-month", 1500)
 
 
-#prepare data
-month_catagory = {"Jan": 0., "Feb": 1., "Mar": 2., "Apr": 3., "May": 4., "Jun": 5., "Jul": 6., 
-                  "Aug": 7., "Sep": 8., "Oct": 9., "Nov": 10., "Dec": 11.}
+def prepare_data(dates, prices, shiller):   
+    month = {"Jan": 0., "Feb": 1., "Mar": 2., "Apr": 3., "May": 4., "Jun": 5., "Jul": 6., "Aug": 7., "Sep": 8., "Oct": 9., "Nov": 10., "Dec": 11.}
+    dates = [month[date.split()[0]] for date in dates]
 
-prices_dates = [month_catagory[date.split()[0]] for date in prices_dates]
+    data = {"Month": dates, "Prices": prices, "Shiller PE": shiller}
+    df = pd.DataFrame(data)
+    df['Prices'] = df['Prices'].pct_change()
+    df.dropna(inplace=True)
+    return df
 
-data = {"month": prices_dates, "prices": prices_values, "shiller PE": shiller_values}
-df = pd.DataFrame(data)
-
-months_ahead = 1
-df['future'] = df['prices'].shift(-months_ahead)
-df[['prices','future']] = df[['prices','future']].pct_change()
-df.dropna(inplace=True)
-df_mean = df.mean()
-df_std = df.std()
-df = (df - df_mean)/df_std
-df.head(3)
+df = prepare_data(prices_dates, prices_values, shiller_values)
 
 
-#convert to numpy dataset
-features = []
-labels = []
-for i in range(len(df)):
-    ds = df.iloc[i].to_list()
-    features.append([ds[0], ds[1], ds[2]])
-    labels.append([ds[3]])
-features = np.array(features)
-labels = np.array(labels)
-features_test = features.reshape(1500,3,1)
-labels_test = labels.reshape(1500,1,1)
-features_test = features_test[:-36]
-labels_test = labels_test[:-36]
+def standardize_data(data):
+    data = (data - data.mean())/data.std()
+    return data
+
+standardized_df = standardize_data(df)    
 
 
-#build model
+def split_data(data):
+    features = data[:,:-1,:]   
+    labels = data[:,-1:, :]  
+    label = tf.stack([labels[:, :, 1]], axis=-1)
+    return features, label
+
+
+def preprocess_data(data, sequence_length = 10):
+    ds = tf.keras.preprocessing.timeseries_dataset_from_array(
+        data = data,
+        targets = None,
+        sequence_length = sequence_length,
+        batch_size= 12,
+        shuffle = False,
+    )
+    return ds.map(split_data)
+
+ds = preprocess_data(standardized_df)
+
+
+def seperate_dataset(data):
+    train = data.take(len(data) - 2)
+    validation = data.skip(len(data) - 2)   
+    return train, validation
+
+train_ds, validation_ds = seperate_dataset(ds)
+
+
 model = tf.keras.models.Sequential()
-model.add(tf.keras.layers.LSTM(128, return_sequences=True, activation='relu'))
-model.add(tf.keras.layers.LSTM(64, return_sequences=False, activation='relu'))
+model.add(tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(64, return_sequences = True, activation = 'relu')))
 model.add(tf.keras.layers.Dense(1))
-model.compile(loss='mse', optimizer='adam', metrics=['mae'])
+model.compile(loss = tf.keras.losses.MeanSquaredError(), optimizer = tf.keras.optimizers.Adam(), metrics = ['mae'])
 
 
-#train model
-model.fit(features_test, labels_test, epochs=5, validation_split=0.05)
+early_stopping = tf.keras.callbacks.EarlyStopping(monitor = 'val_loss', patience = 2, mode = 'min')
+model.fit(train_ds, epochs = 50, validation_data = validation_ds, callbacks = [early_stopping])
 
 
-#graph prediction
-features_prediction = features.reshape(1500,3,1)
+months = 12
+predictions = []
+for i in range(0, months):
+    if i == 0:
+        test_ds = standardized_df[-9:].values.reshape(-1, 9, 3)
+    else:       
+        test_ds = standardized_df[-i-9:-i].values.reshape(-1, 9, 3)
+           
+    prediction = model.predict(test_ds)
+    prediction = prediction*df['Prices'].std() + df['Prices'].mean()
+    prediction = prediction*prices_values[-i-1] + prices_values[-i-1]
+    predictions.append(prediction[0,-1,0])
 
-time=0
-if time != 0:
-    y = prices_values[-(time+12):-time]
-    features_prediction = features_prediction[-(time+1):-time]
-    
-else:
-    y = prices_values[-12:]
-    features_prediction = features_prediction[-1:]
+predictions = predictions[::-1]
 
-prediction_percentage = model.predict(features_prediction)
-percentage_change = prediction_percentage*df_std['prices'] + df_mean['prices']
-prediction = y[-1]*percentage_change + y[-1]
 
-x = range(12)
-plt.scatter(x, y, marker='o')
+plt.scatter(range(0, months - 1), prices_values[-(months - 1):], label='Actual Prices')
+plt.scatter(range(0, months), predictions, label='Prediction')
+plt.title('Stock Market Predictions')
+plt.xlabel('Months')
+plt.ylabel('Prices')
+plt.legend()
+plt.show()
+print(f"Prediction: {predictions[-1]}")
 
-if time != 0:
-    plt.scatter([12], [prices_values[-time]], color='blue')
-else:
-    plt.scatter(None, None, color='blue')
-    
-plt.scatter([12], [prediction], color='green')
-plt.legend(['Historical Prices','Actual Price', 'Predicted Price'])
-plt.title("SP 500")
-plt.xlabel("Month")
-plt.ylabel("Price")
-
-print(f'Predicted percent change: {percentage_change[0][0]*100}')
 
 
 
